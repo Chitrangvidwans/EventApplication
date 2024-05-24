27f9df08ec161011073346573188d59b1bd9c22f
Chitrangvidwans <cvidwans100@gmail.com>
2024-05-24 12:16:02 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 
 import './App.css';
-import Homescreen from './pages/Homescreen';
-import EventForm from './pages/EventForm';
 import { useSession , useSupabaseClient, useSessionContext } from '@supabase/auth-helpers-react';
-import Formyt from './pages/Formyt';
-import { Button} from '@mui/material';
+import React, { useState } from 'react';
+import { TextField, Button, Container, Grid } from '@mui/material';
+import 'react-datepicker/dist/react-datepicker.css';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+
+
 
 
 
 function App() {
+
+    const [eventName, setEventName] = useState('');
+    const [start, setStartDate] = useState(new Date());
+    const [end, setEndDate] = useState(new Date());
+    const [Description, setDescription] = useState('');
+  
+  
+      console.log({
+        eventName,
+        start,
+        end,
+        Description
+      });
+
+
+
+
   const session= useSession();
   const supabase= useSupabaseClient();
 const { isloading } = useSessionContext();
@@ -36,26 +58,29 @@ return <></>
   async function signOut(){
     await supabase.auth.signOut();
   }
-  async function AddEvent() {
+  async function createCalendarEvent() {
     console.log("adding event");
     const event = {
       'summary': 'eventName',
       'description': 'EventDescription',
-      'startDate': {
-        'dateTime': startDate.toISOString(),
-        'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone
+      'start': {
+        'dateTime': `2020-10-31T17:00:00.000`,
+        'timeZone': 'GMT+05:30'//startDate.toISOString(),
+         //Intl.DateTimeFormat().resolvedOptions().timeZone
       }
 ,
-      'endDate': {
-        'dateTime': endDate.toISOString(),
-        'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone
+      'end': {
+        'dateTime': `2020-10-31T17:00:00.000`,
+        'timeZone': 'GMT+05:30'
+        // 'dateTime': endDate.toISOString(),
+        // 'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone
       }
 
     }
-    await fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events',{
-      method: 'POST',
+    await fetch("https://www.googleapis.com/calendar/v3/calendars/primary/events", {
+      method: "POST",
       headers: {
-        'Authorization': `Bearer ${session.access_token}`,
+        'Authorization':'Bearer ' + session.provider_token
       },
       body: JSON.stringify(event)
   
@@ -75,13 +100,58 @@ return <></>
       <>
       <h2>Welcome {session.user.email}</h2> 
       
+
       <Button variant="contained" color='error' onClick={()=> signOut()}>Sign Out</Button>
 
       <hr />
-      <EventForm/>
-      <Button type="submit" variant="contained" color="primary" onClick={()=> AddEvent()}>
+   
+      
+      <Container maxWidth="sm">
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              label="Event Name"
+              fullWidth
+              value={eventName}
+              onChange={(e) => setEventName(e.target.value)}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12}>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DemoContainer components={['DateTimePicker']}>
+        <DateTimePicker disablePast label="Start date " onChange={(date) => setStartDate(date.toDate())} />
+      </DemoContainer>
+    </LocalizationProvider>
+
+    </Grid>
+          <Grid item xs={12} sm={12}>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DemoContainer components={['DateTimePicker']}>
+        <DateTimePicker disablePast label="End date " onChange={(date) => setEndDate(date.toDate())} />
+      </DemoContainer>
+    </LocalizationProvider>
+
+    </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Description"
+              fullWidth
+              value={Description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </Grid>
+          <Grid item xs={12}>
+
+          <Button type="submit" variant="contained" color="primary" onClick={()=> createCalendarEvent()}>
               Submit
             </Button>
+
+          </Grid>
+        </Grid>
+    
+    </Container>
+
+      
       
       </>
       :
